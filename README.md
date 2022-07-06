2266b319af8868040f1c09c044e4ba0ec96dc0f3
Rosulen <camilaojeda97@gmail.com>
2022-07-05 23:52:23 -0500

    :fire::tada: Finished scraping for websites + README update :white_check_mark:

--- README.md
+++ README.md
@@ -1,5 +1,64 @@
 # Data Analytics + Python
 
-Este reto consiste en...
+El objetivo es crear un proyecto que consuma datos desde 3 fuentes distintas para popular una base de datos SQL con información cultural sobre bibliotecas, museos y salas de cines argentinos.
 
-Para obtimizar 
+Este proyecto debe cumplir una seríe de requerimientos funcionales, estos se dividen en 4 partes fundamentales:
+
+**Archivos fuente:** En esta fase se deberan obtener los archivos fuente utilizando la librería requests y serán almacendos de forma local.
+
+**Procesamiento de datos:** En esta fase se procesan los datos para transformar los datos de los archivos fuente en la información que va a nutrir la base de datos.
+
+Y finalmente la creación de las bases de datos y la actualización de estas, las cuales deben cumplir con una serie de requisitos, para más información y detalle de cada fase del proyecto consultar [este enlace](https://drive.google.com/file/d/1ZxBnjsof8yCZx1JVLVaq5DbRjvIIvfJs/view "Detalles de reto Alkemy").
+
+___
+
+# PRIMERA FASE: Archivos fuente
+
+Para empezar con esta fase se consideró importante crear un archivo [txt](./urls.txt) que contenga los enlaces que el programa deberá consultar, para posteriormente descargar los archivos.
+
+en este caso los enlaces a consultar son los siguientes:
+
+- https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d
+
+- https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae
+
+- https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7
+
+
+El programa consulta el archivo txt y guarda en un array los enlaces del archivo, como se muestra en la siguiente imagen, luego se llama a una función con el nombre *download_htmls* para extraer y almacenar en el array **paths** el contenido html de cada enlace.
+
+<p align="center"> <img src=./imagenes/get_html.png> </p>
+
+La funcion *download_htmls* se hace un requests a cada enlace y se almacena en la variable *paths* el contenido html de cada fuente.
+
+<p align="center"> <img src=./imagenes/download_htmls.png> </p>
+
+Ahora la variable *paths* de la imagen anterior se retorna nuevamente a la primera función (get_html).
+
+<p align="center"> <img src=./imagenes/get_html_2.png> </p>
+
+Ahora bien, si nos dirigimos a cada uno de los enlaces, como primra vista encontraremos que hay un boton descargar, el cual directamente va a la descarga del documento base para llenar la base de datos, esto ocurre con todos los enlances.
+
+<p align="center"> <img src=./imagenes/vista_pag.png> </p>
+
+Ahora se consulta el código fuente de la pagina, y se ubica el código del botón verde que indica la descarga en el html, como se observa a continuación.
+
+<p align="center"> <img src=./imagenes/html_pag.png> </p>
+
+Lo que realmente nos interesa es el enlace que directamente nos lleve a la descarga del archivo, entonces mediante el código vamos a ubicar en el html lo que nos interesa.
+
+Antes de continuar, el código debe crear las rutas donde se guardarán los archivos .csv, estos deben quedar en rutas como se indica en el documento. 
+
+<p align="center"> <img src=./imagenes/recorte.png> </p>
+
+Para crear los nombres dependiendo la fecha, se realiza el siguiente código
+
+<p align="center"> <img src=./imagenes/crear_rutas.png> </p>
+
+Finalmente mediante la libreria *BeautifulSoup* se busca en el html el fragmento de código que redirecciona al enlace de la descarga, entonces se agrega la siguiente linea, donde se ezpecifica exactamente que parte del código html necesito buscar.
+
+<p align="center"> <img src=./imagenes/extraer.png> </p>
+
+<p align="center"> <img src=./imagenes/html.png> </p>
+
+Luego se extrae de las "" el enlace y se guarda el documento, y finalmente en los direcotrios mensionados anteriormente se guardan los archivos, el código completo de esta primera fase se encuentra dando click [aquí](./scraper.py). 
--- scraper.py
+++ scraper.py
@@ -6,16 +6,15 @@ from os import path
 from pathlib import PurePath
 from bs4 import BeautifulSoup
 
-import sys, re, requests, os
+import sys, re, requests, os, datetime
 
 counter = 0
 
 def get_html():
 
-    # Se analizan todas las url indicadas en el archivo urls.txt, con el fin de obtimizar variables
+    # Se analizan todas las url indicadas en el archivo urls.txt, con el fin de optimizar variables
     with open('/home/angie/Documents/git/Data_Analytics+Python/urls.txt', 'r') as fh:
         urls = fh.readlines()
-    urls = [url.strip() for url in urls] # Se guarda linea por linea cada enlace del documento
 
     paths = download_html(urls, '.')
     
@@ -27,7 +26,7 @@ def download_html(urls, dir):
 
     for url in urls:
         file_name = PurePath(url).name # Se extrae el ultimo paramatro de la ruta para nombrar el archivo
-        file_path = path.join(dir, file_name) # Se asigna el path
+        file_path = path.join(dir, file_name) # Se asigna el path con el que será guardado
         text = ''
 
         try:
@@ -40,7 +39,7 @@ def download_html(urls, dir):
             print(exc)
     
         with open(file_path, 'w') as fh: # Guardar el archivo con el nombre del path
-            fh.write(text)
+            fh.write(text) # se guarda el contenido html de cada enlace
 
         paths.append(file_path) # Se crea un vector con todos los paths
 
@@ -52,53 +51,54 @@ def get_document(path):
     local_counter = 0
     pos = []
 
-    set_names()
+    paths, names_files = set_names() # Los valores que retornan, retornan a las variables correspondientes
     
     with open(path, 'r') as fh:
         content = fh.read()
-    soup = BeautifulSoup(content, 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser') # Se indica que se analizará un arhivo con contenido html
 
-    content_class = soup.find_all("a", {"class": "btn btn-green btn-block"})
-    content_class = str(content_class)
+    content_class = soup.find_all("a", {"class": "btn btn-green btn-block"}) # se busca la clase btn btn-green btn-block en el archivo
+    content_class = str(content_class) # se convierte a str la linea extraida del html
     local_counter = 0
     pos = []
-    for i, char in enumerate(content_class):
+    for i, char in enumerate(content_class): # como el fragmento que necesitamos está entre "" entones se cuentan y se extrae lo que me sierve
         if char == '"':
             local_counter += 1
             if local_counter >= 3:
                 pos.append(i)
                 if local_counter == 4:
                     break
-    link = content_class[pos[0]+1:pos[1]]
-    print(type(link))
-    print(link)
+    link = content_class[pos[0]+1:pos[1]] # Se extrae dependiendo de la posición
     response = requests.get(link)
-    open(names_databases[counter-1],"wb").write(response.content)
+    final_path = str(paths[counter-1] + '/' + names_files[counter-1]) # se concatena como string el path donde se guardará el archivo
 
-def set_names():
-    local_counter = 0
+    if not os.path.exists(paths[counter-1]): # Si el direcotrio no existe creelo
+        os.makedirs(paths[counter-1])
+        open(final_path,"xb").write(response.content) # se guarda el archivo
+
+    else: # Si los direcotrios existen, solamente se reemplara el archivo
+        open(final_path,"wb").write(response.content)
+    
+
+
+def set_names(): # Asignar el nombre de cada archivo
+    paths = []
+    names_files = []
     names_databases = ["museos", "salas_de_cine", "bibliotecas_populares"]
-    
-    for i in names_databases:
-        os.mkdir()
-    
-    from datetime import datetime
-    date = datetime.now()
-    month = date.strftime('%m')
 
-    import datetime
+    date = datetime.datetime.now()
+    month = date.strftime('%m') # Traer el número del mes actual
     datetime_object = datetime.datetime.strptime(month, "%m")
-    full_month_name = datetime_object.strftime("%B")
+    full_month_name = datetime_object.strftime("%B") # Convertir el número del mes al nombre del mes
 
-
-
-    from datetime import datetime
     for i in names_databases:
-        names_databases[local_counter] = names_databases[local_counter] +"\$"+ date.strftime('%Y-') + full_month_name + names_databases[local_counter] + '-' + date.strftime('%D') + '-' + date.strftime('%M') + '-' + date.strftime('%Y') + '.csv'
-        local_counter += 1
+        paths.append(i + '/' + date.strftime('%Y-') + full_month_name) # crear un vector con la ruta "museos/año-nombre de mes"
+        names_files.append(i + '-' + date.strftime('%d') + '-' + date.strftime('%m') + '-' + date.strftime('%Y') + '.csv') # crear el nombre del archivo .csv
+
+    return paths, names_files # retornar los dos valores anteriores
 
 if __name__ == "__main__":
     args = sys.argv 
 
-    if len(args) is 1:
+    if len(args) == 1:
       get_html()
